eddc10d5a84078495c56d1c132636c16bdcc3db8
Astrik Torres <astriktorres@gmail.com>
2021-07-04 09:24:16 -0500

    Ejercicios resueltos

---
+++ Ejercicios/src/Ejercicio01.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Ejercicio01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		 System.out.println ("Introduzca su nombre:");
+		 String entradaTeclado = "";
+		//Creación de un objeto Scanner
+		 Scanner entradaEscaner = new Scanner (System.in);
+		//Invocamos un método sobre un objeto Scanner
+	     entradaTeclado = entradaEscaner.nextLine();
+	     System.out.println ("Bienvenido " + entradaTeclado);
+	}
+
+}
---
+++ Ejercicios/src/Ejercicio02.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Ejercicio02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println ("Introduzca su nombre:");
+		System.out.println(saludo());
+		
+		
+		
+	}
+
+	public static String saludo() {
+		String entradaTeclado = "";
+		Scanner entradaScanner = new Scanner (System.in);
+		entradaTeclado = entradaScanner.nextLine();
+		return "Hola, " + entradaTeclado;
+	}
+	
+}
